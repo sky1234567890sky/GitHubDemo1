d15b738b50ee3275f1c329a80deaa6ac8b5149c3
sky1234567890sky <sky1234567890sky>
2019-08-02 10:36:58 +0800

    苏克阳1

---
+++ app/src/main/java/com/example/githubdemo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.githubdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "我是苏克阳", Toast.LENGTH_SHORT).show();
+    }
+}
